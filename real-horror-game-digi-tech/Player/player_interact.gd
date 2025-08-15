1b9cbfbe2f19fb41dece2a95774bb2653543e69d
JakeIsKale <jakeclarkthompson@gmail.com>
2025-08-15 13:29:33 +1200

    nearly finished a lightswtich code

--- real-horror-game-digi-tech/Player/player_interact.gd
+++ real-horror-game-digi-tech/Player/player_interact.gd
@@ -3,6 +3,9 @@ extends RayCast3D
 func _physics_process(delta: float) -> void:
 	if is_colliding():
 		var hit = get_collider()
-		if hit.name == "door":
+		if hit.name == "lightswitch":
+			if Input.is_action_just_pressed("interact"):
+				if hit.name == "door":
+					pass
 			if Input.is_action_just_pressed("interact"):
 				hit.get_parent().get_parent().get_parent().toggle_door()
