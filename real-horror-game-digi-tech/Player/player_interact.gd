d096377a73f6515b5d24c8a83b498d82d2efbb94
JakeIsKale <jakeclarkthompson@gmail.com>
2025-08-18 14:39:46 +1200

    finally finished a lightswitch + started making bathtub on blender + changed world environment

--- real-horror-game-digi-tech/Player/player_interact.gd
+++ real-horror-game-digi-tech/Player/player_interact.gd
@@ -3,9 +3,9 @@ extends RayCast3D
 func _physics_process(delta: float) -> void:
 	if is_colliding():
 		var hit = get_collider()
-		if hit.name == "lightswitch":
-			if Input.is_action_just_pressed("interact"):
-				if hit.name == "door":
-					pass
-			if Input.is_action_just_pressed("interact"):
-				hit.get_parent().get_parent().get_parent().toggle_door()
+		print(hit)
+		#if hit.name == "lightswitch":
+		if hit.name == "lightswitch" and Input.is_action_just_pressed("interact"):
+			hit.get_parent().toggle_light()
+		elif hit.name == "door" and Input.is_action_just_pressed("interact"):
+			hit.get_parent().get_parent().get_parent().toggle_door()
--- real-horror-game-digi-tech/Scripts/lightswitch.gd
+++ real-horror-game-digi-tech/Scripts/lightswitch.gd
@@ -3,23 +3,31 @@ extends Node3D
 @export var on = false
 @export var on_mat: StandardMaterial3D
 @export var off_mat: StandardMaterial3D
-@export var light_bulb: Node3D
+@export var light: Node3D
+@export var lights: Node
 
 func _ready():
-	if !on:
-		light_bulb.get_node("lightbulb").material_override = off_mat
+	var light_group = lights.get_children()
+	if not on:
+		for bulb in light_group:
+			bulb.get_node("light").material_override = off_mat
 	if on:
-		light_bulb.get_node("lightbulb").material_override = on_mat
-	light_bulb.get_node("OmniLight3D").visible = on
+		for bulb in light_group:
+			bulb.get_node("light").material_override = on_mat
+	#light.get_node("OmniLight3D").visible = on
 
 func toggle_light():
+	var light_group = lights.get_children()
 	on = !on
 	if on:
 		$on.visible = true
 		$off.visible = false
-		light_bulb.get_node("lightbulb").material_override = on_mat
+		for bulb in light_group:
+			bulb.get_node("light").material_override = on_mat
+			bulb.get_node("OmniLight3D").visible = on
 	if !on:
 		$on.visible = false
 		$off.visible = true
-		light_bulb.get_node("lightbulb").material_override = off_mat
-	light_bulb.get_node("OmniLight3D").visible = on
+		for bulb in light_group:
+			bulb.get_node("light").material_override = off_mat
+			bulb.get_node("OmniLight3D").visible = on
