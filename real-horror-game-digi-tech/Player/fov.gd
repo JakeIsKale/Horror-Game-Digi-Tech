2980625399f8190ce162dc7aedb7b8f575d1f841
JakeIsKale <jakeclarkthompson@gmail.com>
2025-09-18 10:09:53 +1200

    made a bin and bush, fixed some bugs

--- real-horror-game-digi-tech/Player/fov.gd
+++ real-horror-game-digi-tech/Player/fov.gd
@@ -1,11 +1,12 @@
 extends Camera3D
 
-var wanted_fov = 75 
+var wanted_fov: float = 75.0 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	if Input.is_action_just_pressed("zoom") and wanted_fov != 30:
-		wanted_fov = 30 
-	if !Input.is_action_just_pressed("zoom") and wanted_fov != 75:
-		wanted_fov = 75
+	if Input.is_action_pressed("zoom") and wanted_fov != 30.0:
+		wanted_fov = 30.0
+	if !Input.is_action_pressed("zoom") and wanted_fov != 75.0:
+		wanted_fov = 75.0
 	var wanted_value = lerp(fov, wanted_fov, 0.2)
+	fov = wanted_value
